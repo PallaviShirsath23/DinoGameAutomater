6b13f613cce3bd9d0620d7cb97407f3989309ff2
PallaviShirsath23 <pallavishirsath2024@gmail.com>
2025-01-30 23:55:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pyautogui # pip install pyautogui
 from PIL import Image, ImageGrab # pip install pillow
 # from numpy import asarray
-import time
+import 
 
 def hit(key):
     pyautogui.keyDown(key)
